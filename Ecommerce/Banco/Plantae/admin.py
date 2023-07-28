1b89d1ce989f3e3e516acef9cde505425f6d870d
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2023-07-28 16:45:31 -0300

    Delete Ecommerce directory

--- Ecommerce/Banco/Plantae/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-
-@admin.register(models.Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ['nomeCompleto', 'username', 'data_nascimento', 'cpf', 'numero_telefone', 'email', 'password']
--- Ecommerce/Site/src/componentes/Borda.jsx
+++
@@ -1,9 +0,0 @@
-const Borda = () => {
-    return (
-        <div className="w-full flex justify-center">
-            <div className="border-t border-borda w-[95%]" />
-        </div>
-    )
-}
-
-export default Borda
